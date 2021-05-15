2e6e5fdcfe5cc1fcc2d7709c891c72ee2762c0a4
fangyu <susie.salmon.1973.12.06@gmail.com>
2021-05-15 21:28:13 +0800

    fix hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import CountyWeather from '@/views/CountyPage.vue'
 import TownPage from '@/views/TownPage.vue'
 
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 })
 
